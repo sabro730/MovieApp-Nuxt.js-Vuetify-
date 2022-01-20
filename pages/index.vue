bf616ccf900b7bd04f2f4ad77f050c1ca7f8513d
golden-lib <tousei.sasaki216@gmail.com>
2022-01-20 23:22:55 +0300

    feat: fetch popular movies in store

--- pages/index.vue
+++ pages/index.vue
@@ -81,7 +81,33 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
   name: 'IndexPage',
+
+  data() {
+    return {
+      //
+    }
+  },
+
+  async fetch() {
+    await this.fetchPopularMovies()
+  },
+
+  computed: {
+    ...mapGetters({
+      popularMovies: 'popularMovies',
+    }),
+  },
+
+  created() {},
+
+  methods: {
+    ...mapActions({
+      fetchPopularMovies: 'fetchPopularMovies',
+    }),
+  },
 }
 </script>
---
+++ store/index.js
@@ -0,0 +1,22 @@
+export const state = () => ({
+    popularMovies: [],
+})
+
+export const mutations = {
+    SET_POPULAR_MOVIES(state, payload) {
+        console.log("SET_POPULAR_MOVIES", payload)
+        state.popularMovies = payload
+    }
+}
+
+export const actions = {
+    async fetchPopularMovies({ commit }) {
+        await this.$axios.get('movie/popular' + process.env.API_KEY).then(response => {
+            commit('SET_POPULAR_MOVIES', response.data.results);
+        })
+    },
+}
+
+export const getters = {
+    popularMovies: state => state.popularMovies,
+}
