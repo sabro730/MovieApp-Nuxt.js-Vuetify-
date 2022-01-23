0b58c8d2aac3097b1501815dc87a742abbcdff70
golden-lib <tousei.sasaki216@gmail.com>
2022-01-23 14:52:58 +0300

    feat: nav dropdown menu

---
+++ components/NavigationDropdownMenu.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-menu offset-y>
+    <template #activator="{ on: menu }">
+      <v-btn large text v-on="{ ...menu }">
+          Menu
+      </v-btn>
+    </template>
+
+    <v-list dense>
+      <v-list-item  v-for="item in items" :key="item" :to="item.to">
+        <v-list-item-title >{{ item.title }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      {
+        icon: 'mdi-apps',
+        title: 'Welcome',
+        to: '/',
+      },
+      {
+        icon: 'mdi-chart-bubble',
+        title: 'Inspire',
+        to: '/inspire',
+      },
+    ],
+  }),
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,44 +1,33 @@
 <template>
   <v-app dark>
-    <v-app-bar :clipped-left="clipped" fixed app dense>
+    <v-app-bar :clipped-left="clipped" fixed app dense color="purple darken-4">
       <v-btn class="unfocusableButton" text to="/">
         <nuxt-logo></nuxt-logo>
         <h1 class="ml-4">Kinomax</h1>
         <h6 class="mt-6 ml-2">by mokumus &copy;</h6>
       </v-btn>
-      <v-spacer />
-      <v-btn> Home </v-btn>
+      <v-spacer></v-spacer>
+      <navigation-dropdown-menu/> 
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer app>
+    <v-footer app color="purple darken-4" max-height="27px">
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+
 export default {
   name: 'DefaultLayout',
   data() {
     return {
       clipped: false,
       drawer: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
@@ -51,5 +40,15 @@ export default {
 <style scoped>
 .unfocusableButton.v-btn--active::before {
   opacity: 0 !important;
+}
+
+.v-application {
+  background: url("./static/bg.png") no-repeat center center fixed !important;
+  background-size: cover;
+}
+
+.v-application .purple.darken-4 {
+    background-color: #4a148c6e !important;
+    border-color: #4a148c !important;
 }
 </style>
