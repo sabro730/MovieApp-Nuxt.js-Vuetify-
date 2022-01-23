48dc59a3ff027fa20f44969bfa955db16b0e8dc9
golden-lib <tousei.sasaki216@gmail.com>
2022-01-23 18:38:07 +0300

    fix: drop console on build

--- nuxt.config.js
+++ nuxt.config.js
@@ -79,5 +79,13 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    terser: {
+      terserOptions: {
+        compress: {
+          drop_console: true,
+        },
+      },
+    },
+  },
 }
