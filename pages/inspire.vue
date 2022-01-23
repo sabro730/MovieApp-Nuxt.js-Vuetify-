6af5e484cca2743f91049d9ecd10bc275056f7a4
golden-lib <tousei.sasaki216@gmail.com>
2022-01-23 17:59:08 +0300

    chore: github links

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -9,6 +9,13 @@
             <em>&mdash;John Johnson</em>
           </small>
         </footer>
+
+                    <a
+              :href="`https://github.com/mokumus/MovieApp`"
+              target="_blank"
+            >
+            <h4> https://github.com/mokumus/MovieApp </h4>
+            </a>
       </blockquote>
     </v-col>
   </v-row>
@@ -19,3 +26,11 @@ export default {
   name: 'InspirePage',
 }
 </script>
+
+<style>
+h4 {
+  font-size: 1.5rem;
+  font-weight: 500;
+  color: #2f32dd;
+}
+</style>
